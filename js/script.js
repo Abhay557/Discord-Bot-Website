d3654ede8028c81d18d70e31406a5a7b24d3c124
Abhay <abhaycormourya@gmail.com>
2024-10-11 09:26:37 +0000

    done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-console.log("make by Abhay557 - source code available on github (Abhay557)");
+console.log("made by Abhay557 - source code available on github (Abhay557)");
 
 setInterval(() => {
-	console.log("make by Abhay557 - source code available on github (Abhay557)");
+	console.log("made by Abhay557 - source code available on github (Abhay557)");
 }, 10000)
 
 let nt = document.getElementById("toggler");
@@ -34,80 +34,3 @@ window.onload = () => {
 		body.classList.remove("on")
 	}, 1510);
 }
-
-function Ticker( elem ) {
-	elem.lettering();
-	this.done = false;
-	this.cycleCount = 5;
-	this.cycleCurrent = 0;
-	this.chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()-_=+{}|[]\\;\':"<>?,./`~'.split('');
-	this.charsCount = this.chars.length;
-	this.letters = elem.find( 'span' );
-	this.letterCount = this.letters.length;
-	this.letterCurrent = 0;
-
-	this.letters.each( function() {
-		var $this = $( this );
-		$this.attr( 'data-orig', $this.text() );
-		$this.text( '-' );
-	});
-}
-
-Ticker.prototype.getChar = function() {
-	return this.chars[ Math.floor( Math.random() * this.charsCount ) ];
-};
-
-Ticker.prototype.reset = function() {
-	this.done = false;
-	this.cycleCurrent = 0;
-	this.letterCurrent = 0;
-	this.letters.each( function() {
-		var $this = $( this );
-		$this.text( $this.attr( 'data-orig' ) );
-		$this.removeClass( 'done' );
-	});
-	this.loop();
-};
-
-Ticker.prototype.loop = function() {
-	var self = this;
-
-	this.letters.each( function( index, elem ) {
-		var $elem = $( elem );
-		if( index >= self.letterCurrent ) {
-			if( $elem.text() !== ' ' ) {
-				$elem.text( self.getChar() );
-				$elem.css( 'opacity', Math.random() );
-			}
-		}
-	});
-
-	if( this.cycleCurrent < this.cycleCount ) {
-		this.cycleCurrent++;
-	} else if( this.letterCurrent < this.letterCount ) {
-		var currLetter = this.letters.eq( this.letterCurrent );
-		this.cycleCurrent = 0;
-		currLetter.text( currLetter.attr( 'data-orig' ) ).css( 'opacity', 1 ).addClass( 'done' );
-		this.letterCurrent++;
-	} else {
-		this.done = true;
-	}
-
-	if( !this.done ) {
-		requestAnimationFrame( function() {
-			self.loop();
-		});
-	} else {
-		setTimeout( function() {
-			self.reset();
-		}, 750 );
-	}
-};
-
-var $words = $( '.word' );
-
-$words.each( function() {
-	var $this = $( this ),
-		ticker = new Ticker( $this ).reset();
-	$this.data( 'ticker', ticker  );
-});
